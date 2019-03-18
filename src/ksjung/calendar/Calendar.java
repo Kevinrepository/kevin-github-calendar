be1e21ea2cf31d92e76c37fc6f103bed6179297c
office <office@175.196.62.48>
2019-03-18 18:42:20 +0900

    secont commit

--- src/ksjung/calendar/Calendar.java
+++ src/ksjung/calendar/Calendar.java
@@ -4,7 +4,12 @@ public class Calendar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, Calendar.");
+		System.out.println("일    월    화   수    목   금    토");
+		System.out.println("--------------------");
+		System.out.println("                1  2");
+		System.out.println(" 3  4  5  6  7  8  9");
+		System.out.println("10 11 12 13 14 15 16");
+		
 	}
 
 }
