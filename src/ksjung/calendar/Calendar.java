2afaa513953149210ea1c6981213a61b9ea3c830
office <office@175.196.62.48>
2019-03-19 11:58:16 +0900

    calendar march

--- src/ksjung/calendar/Calendar.java
+++ src/ksjung/calendar/Calendar.java
@@ -1,5 +1,7 @@
 package ksjung.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
@@ -9,6 +11,13 @@ public class Calendar {
 		System.out.println("                1  2");
 		System.out.println(" 3  4  5  6  7  8  9");
 		System.out.println("10 11 12 13 14 15 16");
+		System.out.println("17 18 19 20 21 22 23");
+		System.out.println("24 25 26 27 28 29 30");
+		System.out.println("31                  ");
+		
+		// maximum dates of input month
+		Scanner scanner = new Scanner(System.in);
+		int result = scanner.nextInt();
 		
 	}
 
