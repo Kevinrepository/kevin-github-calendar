97916c495229f7c670025657111ba04ee33bcb3f
office <office@175.196.62.48>
2019-03-19 12:02:39 +0900

    Implement find max days of the month

--- src/ksjung/calendar/Calendar.java
+++ src/ksjung/calendar/Calendar.java
@@ -17,8 +17,11 @@ public class Calendar {
 		
 		// maximum dates of input month
 		Scanner scanner = new Scanner(System.in);
-		int result = scanner.nextInt();
+		System.out.print("Input month: ");
+		int month = scanner.nextInt();
 		
+		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		System.out.printf("Month %d has %d days\n", month, maxDays[month]);
 	}
 
 }
