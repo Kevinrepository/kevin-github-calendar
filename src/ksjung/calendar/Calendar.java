ea811a0654ae08b877c94dcc7d5bbe7d90084256
office <office@175.196.62.48>
2019-03-19 12:09:30 +0900

    Updating calculator.

--- src/ksjung/calendar/Calendar.java
+++ src/ksjung/calendar/Calendar.java
@@ -7,9 +7,8 @@ public class Calendar {
 	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	
 	public int getMaxDaysOfMonth(int month) { return MAX_DAYS[month - 1]; }
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	private void printSampleCalendar() {
 		System.out.println("일    월    화   수    목   금    토");
 		System.out.println("--------------------");
 		System.out.println("                1  2");
@@ -18,6 +17,10 @@ public class Calendar {
 		System.out.println("17 18 19 20 21 22 23");
 		System.out.println("24 25 26 27 28 29 30");
 		System.out.println("31                  ");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		
 		// maximum dates of input month
 		Scanner scanner = new Scanner(System.in);
@@ -26,6 +29,10 @@ public class Calendar {
 		int month = scanner.nextInt();
 		
 		System.out.printf("Month %d has %d days\n", month, cal.getMaxDaysOfMonth(month));
+		
+		cal.printSampleCalendar();
+		
+		scanner.close();
 	}
 
 }
